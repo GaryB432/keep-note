6ad2a39909486aaa58463ce85233ea09b8767750
Gary Bortosky <997067+GaryB432@users.noreply.github.com>
2026-03-27 09:31:38 -0500

    refactor: enhance Note type definition and remove unused reader components

--- bin.js
+++ bin.js
@@ -49,61 +49,37 @@ cli
 cli
   .command("takeout [dir]", "Created GFM Documents from Google Takeout")
   .action(async (dir, options) => {
-    const nns = await digest(dir);
-    const nnt = nns.map((n) =>
-      n.title ?? n.textContent
-        .concat(" there>")
-        .concat(new Date(n.createdTimestampUsec).toISOString()),
-    );
-    log.message(nnt);
-    // for (const note of await digest(dir)) {
-    //   // log.message(note);
-    // }
-    // console.log(
-    //   JSON.stringify(
-    //     m.map((q) => ({ f: q.title })),
-    //     undefined,
-    //     6,
-    //   ),
-    // );
+    const allNotes = await digest(dir);
 
-    // const nameInput = name ?? (await text({ message: "What is your name?" }));
-    // if (isCancel(nameInput)) {
+    const labelsUsed = allNotes.reduce((a, note) => {
+      if (note.labels) {
+        note.labels.forEach((l) => a.add(l.name));
+      }
+
+      return a;
+    }, new Set());
+
+    const fun = await select({
+      message: "skip any labels",
+      options: Array.from(labelsUsed.values()).map(d=> ({value: d})),
+    });
+
+    // const tz =
+    //   timezone ??
+    //   (await select({
+    //     message: "Select your timezone",
+    //     options: zones
+    //       .filter((z) => z.startsWith("America"))
+    //       .map((z) => ({ value: z, label: z })),
+    //   }));
+    // if (isCancel(tz)) {
     //   cancel("Cancelled.");
     //   process.exit(0);
     // }
+
+    // console.log(JSON.stringify(nns, undefined, 4));
 
     log.success(`${dir} made it`);
   });
 
 cli.parse();
-
-const m = [
-  {
-    color: "DEFAULT",
-    isTrashed: false,
-    isPinned: false,
-    isArchived: false,
-    textContent: "Open cell ID",
-    title: "",
-    userEditedTimestampUsec: 1763754124984000,
-    createdTimestampUsec: 1763754124984000,
-    textContentHtml: `<p dir="ltr" style="line-height:1.38;margin-top:0.0pt;margin-bottom:0.0pt;"><span style="font-size:7.2pt;font-family:'Google Sans';color:#000000;background-color:transparent;font-weight:400;font-style:normal;font-variant:normal;text-decoration:none;vertical-align:baseline;white-space:pre;white-space:pre-wrap;">Open cell ID</span></p>`,
-  },
-  {
-    color: "DEFAULT",
-    isTrashed: false,
-    isPinned: false,
-    isArchived: true,
-    textContent:
-      "Steve Gibson was talking about the reporters who left Russia and moved to Europe because they were not allowed to report you know honestly or quote on quote but was is it always bad the journalists The forbidden from reporting certain destructive or degenerate things\n" +
-      "\n" +
-      "there is much to think about here\n" +
-      "\n" +
-      "thanks for your thoughts gar",
-    title: "",
-    userEditedTimestampUsec: 1764095324187000,
-    createdTimestampUsec: 1763666278014000,
-    textContentHtml: `<p dir="ltr" style="line-height:1.38;margin-top:0.0pt;margin-bottom:0.0pt;"><span style="font-size:7.2pt;font-family:'Google Sans';color:#000000;background-color:transparent;font-weight:400;font-style:normal;font-variant:normal;text-decoration:none;vertical-align:baseline;white-space:pre;white-space:pre-wrap;">Steve Gibson was talking about the reporters who left Russia and moved to Europe because they were not allowed to report you know honestly or quote on quote but was is it always bad the journalists The forbidden from reporting certain destructive or degenerate things</span></p><br /><p dir="ltr" style="line-height:1.38;margin-top:0.0pt;margin-bottom:0.0pt;"><span style="font-size:7.2pt;font-family:'Google Sans';color:#000000;background-color:transparent;font-weight:400;font-style:normal;font-variant:normal;text-decoration:none;vertical-align:baseline;white-space:pre;white-space:pre-wrap;">there is much to think about here</span></p><br /><p dir="ltr" style="line-height:1.38;margin-top:0.0pt;margin-bottom:0.0pt;"><span style="font-size:7.2pt;font-family:'Google Sans';color:#000000;background-color:transparent;font-weight:400;font-style:normal;font-variant:normal;text-decoration:none;vertical-align:baseline;white-space:pre;white-space:pre-wrap;">thanks for your thoughts gar</span></p>`,
-  },
-];
